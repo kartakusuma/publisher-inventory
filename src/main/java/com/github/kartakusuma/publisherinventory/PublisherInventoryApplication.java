aa8d83baa32462d40a0472f20eeb24fb66f78d6c
Karta Kusuma <kartakusuma1199@gmail.com>
2023-09-15 22:35:53 +0700

    initializing project

---
+++ src/main/java/com/github/kartakusuma/publisherinventory/PublisherInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.github.kartakusuma.publisherinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublisherInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublisherInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/kartakusuma/publisherinventory/PublisherInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kartakusuma.publisherinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublisherInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
