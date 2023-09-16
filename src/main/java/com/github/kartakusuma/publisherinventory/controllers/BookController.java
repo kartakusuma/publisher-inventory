1c5f349d0ae4f588a4668bdee7693ccaed4e1c46
Karta Kusuma <kartakusuma1199@gmail.com>
2023-09-16 18:21:16 +0700

    add author checking in create and update methods

--- src/main/java/com/github/kartakusuma/publisherinventory/controllers/BookController.java
+++ src/main/java/com/github/kartakusuma/publisherinventory/controllers/BookController.java
@@ -14,16 +14,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.kartakusuma.publisherinventory.entities.Author;
 import com.github.kartakusuma.publisherinventory.entities.Book;
+import com.github.kartakusuma.publisherinventory.services.AuthorService;
 import com.github.kartakusuma.publisherinventory.services.BookService;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
     private final BookService bookService;
+    private final AuthorService authorService;
 
-    public BookController(BookService bookService) {
+    public BookController(BookService bookService, AuthorService authorService) {
         this.bookService = bookService;
+        this.authorService = authorService;
     }
 
     @GetMapping("")
@@ -41,12 +45,27 @@ public class BookController {
 
     @PostMapping("")
     public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        Author author = authorService.getAuthorById(book.getAuthor().getId()).orElse(null);
+
+        if (author == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        book.setAuthor(author);
         Book createdBook = bookService.createBook(book);
         return new ResponseEntity<>(createdBook, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book book) {
+        Author author = authorService.getAuthorById(book.getAuthor().getId()).orElse(null);
+
+        if (author == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        book.setAuthor(author);
+
         try {
             Book updatedBook = bookService.updateBook(id, book);
             return new ResponseEntity<>(updatedBook, HttpStatus.OK);
