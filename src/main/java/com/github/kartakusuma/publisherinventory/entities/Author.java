760a61ee4cd1b375360f393c38d0bffb7d5da63a
Karta Kusuma <kartakusuma1199@gmail.com>
2023-09-16 18:20:09 +0700

    fixing relation one to many

--- src/main/java/com/github/kartakusuma/publisherinventory/entities/Author.java
+++ src/main/java/com/github/kartakusuma/publisherinventory/entities/Author.java
@@ -1,9 +1,15 @@
 package com.github.kartakusuma.publisherinventory.entities;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,4 +26,8 @@ public class Author {
     private Long id;
     private String name;
     private String email;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private List<Book> books;
 }
