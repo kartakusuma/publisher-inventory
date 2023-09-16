364e1e2ef5a1e21d2ea1a8787b1ef800e169cecf
Karta Kusuma <kartakusuma1199@gmail.com>
2023-09-16 18:20:29 +0700

    fixing relation many to one

--- src/main/java/com/github/kartakusuma/publisherinventory/entities/Book.java
+++ src/main/java/com/github/kartakusuma/publisherinventory/entities/Book.java
@@ -1,11 +1,13 @@
 package com.github.kartakusuma.publisherinventory.entities;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,6 +24,9 @@ public class Book {
     private Long id;
     private String title;
     private String description;
-    @JsonProperty("author_id")
-    private String authorId;
+    
+    @ManyToOne
+    @JsonBackReference
+    @JoinColumn(name = "author_id")
+    private Author author;
 }
