76562c82a9cdcbf1c29b8ac88aeaa3f5fae2047b
Karta Kusuma <kartakusuma1199@gmail.com>
2023-09-16 16:49:16 +0700

    change book entity schema

--- src/main/java/com/github/kartakusuma/publisherinventory/entities/Book.java
+++ src/main/java/com/github/kartakusuma/publisherinventory/entities/Book.java
@@ -1,11 +1,11 @@
 package com.github.kartakusuma.publisherinventory.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,8 +22,6 @@ public class Book {
     private Long id;
     private String title;
     private String description;
-
-    @ManyToOne
-    @JoinColumn(name = "author_id")
-    private Author author;
+    @JsonProperty("author_id")
+    private String authorId;
 }
